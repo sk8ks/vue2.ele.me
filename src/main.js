ef7463f243793afe5ca0cf946e198ce0e84b3e3a
sk8ks <you@example.com>
2017-07-26 14:24:32 +0800

    插件调整

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import infiniteScroll from 'vue-infinite-scroll'
 import App from './App'
 import router from './router'
 import store from './store'
@@ -9,7 +8,6 @@ import fastclick from 'fastclick'
 import './config/normalize'
 
 
-Vue.use(infiniteScroll)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
