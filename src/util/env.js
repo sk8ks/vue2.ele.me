d6013e6e0b6853fb8954da824080bbeaa6dde2ce
sk8ks <you@example.com>
2017-08-01 02:55:16 +0800

    文件迁移

--- src/util/env.js
+++ src/util/env.js
@@ -3,22 +3,24 @@
  *
  * baseUrl: 域名地址
  * routerMode: 路由模式
- * imgBaseUrl: 图片所在域名地址
- *
+ * staticUrl: 静态资源域名地址
+ * apiUrl: 接口域名地址
  */
-
-let baseUrl = 'http://127.0.0.1:8080';
+let baseUrl = '';
+let apiUrl = '';
 let routerMode = 'history';
-let imgBaseUrl = '';
+let staticUrl = '';
 
 if (process.env.NODE_ENV == 'development') {
 
-}else if(process.env.NODE_ENV == 'production'){
+}
+else if(process.env.NODE_ENV == 'production'){
 
 }
 
 export {
 	baseUrl,
 	routerMode,
-	imgBaseUrl,
+	staticUrl,
+	apiUrl
 }
