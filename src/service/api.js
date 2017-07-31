2e5f2e431b14cd19896aa944776d9c2049378193
sk8ks <you@example.com>
2017-08-01 03:01:10 +0800

    本地数据接口更新
    本地数据接口更新

--- src/service/api.js
+++ src/service/api.js
@@ -1,7 +1,9 @@
+import {apiUrl} from '@/util/env'
 export default {
-    reverseGeoCoding: '/api/reverse_geo_coding',    // 获取当前地理信息
-    entries: '/api/entries',    // 快捷入口
-    hotSearchWords: '/api/hot_search_words',    // 热门词
-    restaurants: '/api/restaurants',    // 餐饮店
-    searchAddressNearby: '/api/search_address_nearby',    // 附近地区
+    reverseGeoCoding:       apiUrl + '/api/reverse_geo_coding',    // 获取当前地理信息
+    entries:                apiUrl + '/api/entries',    // 快捷入口
+    hotSearchWords:         apiUrl + '/api/hot_search_words',    // 热门词
+    restaurants:            apiUrl + '/api/restaurants',    // 餐饮店
+    searchAddressNearby:    apiUrl + '/api/search_address_nearby',    // 附近地区
+    category:               apiUrl + '/api/category',    // 分类数据
 }
--- src/service/index.js
+++ src/service/index.js
@@ -37,7 +37,7 @@ const f = async (url = '', data = {}, type = 'GET') => {
     try {
         const res = await fetch(url, requestConfig);
         const resJson = await res.json();
-        return resJson
+        return resJson;
     } catch (error) {
         throw new Error(error)
     }
@@ -46,8 +46,9 @@ const f = async (url = '', data = {}, type = 'GET') => {
 
 // export const getUserInfo = () => fetch(url.userInfoAction, postJSONConfig({uid: getStore('uid')}));
 //
-export const fetchGeolocation = query => f(api.reverseGeoCoding, query);
-export const fetchEntries = query => f(api.entries, query);
-export const fetchHotSearchWords = query => f(api.hotSearchWords, query);
-export const fetchRestaurants = query => f(api.restaurants, query);
-export const fetchAddressNearby = query => f(api.searchAddressNearby, query);
+export const fetchGeolocation       = query => f(api.reverseGeoCoding, query);
+export const fetchEntries           = query => f(api.entries, query);
+export const fetchHotSearchWords    = query => f(api.hotSearchWords, query);
+export const fetchRestaurants       = query => f(api.restaurants, query);
+export const fetchAddressNearby     = query => f(api.searchAddressNearby, query);
+export const fetchCategory          = query => f(api.category, query);
