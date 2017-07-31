2b6ce3e7c5cdf32f8a47e4fcbf797db325d56d22
sk8ks <you@example.com>
2017-08-01 03:12:34 +0800

    混合方法更新
    混合方法更新

--- src/components/mixin/index.js
+++ src/components/mixin/index.js
@@ -3,7 +3,11 @@ import {geoLocation} from './geo'
 
 export default {
     methods: {
-        geoLocation: geoLocation
+        geoLocation: geoLocation,
+        noScroll (flag) {
+            // 有遮罩层情况下禁止背景内容滚动
+            document.querySelector('body').className = flag ? 'no-scroll' : '';
+        }
     }
 
 }
