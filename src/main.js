aa01d2b819be0455e26cfb6e0964977287ff27d2
sk8ks <you@example.com>
2017-08-15 02:27:30 +0800

    入口文件更新
    入口文件更新，加入表单验证插件

--- src/main.js
+++ src/main.js
@@ -2,20 +2,20 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 // import infiniteScroll from 'vue-infinite-scroll'
+import VeeValidate from 'vee-validate'
 import App from './App'
 import router from './router'
 import store from './store'
 import './util/normalize'
 
-
+Vue.use(VeeValidate)
 // Vue.use(infiniteScroll)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-	// el: '#app',
 	router,
 	store,
 	// render: h => h(App)
 })
-.$mount('#app')
+.$mount('router-view')
